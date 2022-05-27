47289945f225af16069568b600f31954dd72b114
MrGultekin <gultekin.de@gmail.com>
2022-05-27 10:04:21 +0200

    StrictMode added packages deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
+import {StrictMode} from "react";
 const App = () => {
   return (
+      <StrictMode>
     <div>
       <h1>Adopt Me!</h1>
     <SearchParams/>
     </div>
+      </StrictMode>
+
   );
 };
 
